d43d5b1234a8b60ad1a1bed7d2123fd1873489c2
shnagoor <nagoorvali25@gmail.com>
2021-12-07 16:54:57 +0530

    nav gaph

--- app/src/main/java/com/example/gitdemo/Home.kt
+++ app/src/main/java/com/example/gitdemo/Home.kt
@@ -2,11 +2,13 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 
-class Home : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+class Home : Fragment(R.layout.activity_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 
     }
-}
