276ae620bda7a2158cbfcfbadb9758bb77807e12
shnagoor <nagoorvali25@gmail.com>
2021-12-13 12:40:26 +0530

    commit navigation home to login

---
+++ app/src/main/java/com/example/gitdemo/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.gitdemo
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    lateinit var navController:NavController
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        login_btn.setOnClickListener {
+        //    findNavController().navigate(HomeFra)
+           findNavController().navigate(R.id.action_home4_to_loginFragment)
+
+        }
+    }
+
+
+    }
+k
